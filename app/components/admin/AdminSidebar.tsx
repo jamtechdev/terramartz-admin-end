926a806aa8d04552e2a96ea7735e718bdaaa153d
jamtechdev <jamtechba@gmail.com>
2026-02-24 18:46:02 +0530

    remove the report page

--- app/components/admin/AdminSidebar.tsx
+++ app/components/admin/AdminSidebar.tsx
@@ -113,12 +113,12 @@ export default function AdminSidebar({ mobileOpen, setMobileOpen }: Props) {
       icon: <RiTicketLine size={20} />,
       requiredModule: "Support",
     },
-      // {
-      //   name: "Reports",
-      //   href: "/admin/reports",  
-      //   icon: <RiBarChartLine size={20} />,
-      //   requiredModule: "Dashboard",
-      // },
+    // {
+    //   name: "Reports",
+    //   href: "/admin/reports",
+    //   icon: <RiBarChartLine size={20} />,
+    //   requiredModule: "Dashboard",
+    // },
     {
       name: "Logs",
       href: "/admin/logs",
@@ -156,7 +156,7 @@ export default function AdminSidebar({ mobileOpen, setMobileOpen }: Props) {
 
   const SidebarContent = (
     <aside
-      className={`flex flex-col bg-[linear-gradient(rgb(0_130_54),rgb(0_130_54_/_70%)),url('/images/inner-bg.png')] bg-cover bg-center bg-no-repeat text-white p-4
+      className={`flex flex-col bg-[linear-gradient(rgb(0_130_54),rgb(0_130_54/70%)),url('/images/inner-bg.png')] bg-cover bg-center bg-no-repeat text-white p-4
       ${collapsed ? "w-20" : "w-72"} h-full transition-all`}
     >
       {/* Header */}
